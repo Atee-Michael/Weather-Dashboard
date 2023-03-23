478280bfe586dc9c652e0f95a9c510d475d2070d
unknown <ateemichael@yahoo.com>
2023-03-23 20:32:45 +0000

    defined variables

--- JS/script.js
+++ JS/script.js
@@ -1,6 +1,16 @@
 // JavaScript for Weather Dashboard
 
-// Global Variables
+// API key
+const apiKey = "aedd7767a4afa18221b35dcee4cd935c";
+
+// Get the search form, search input, and search history elements
+const searchForm = document.getElementById("search-form");
+const searchInput = document.getElementById("search-input");
+const searchHistory = document.getElementById("history");
+
+// Get the weather sections
+const todayWeather = document.getElementById("today");
+const forecastWeather = document.getElementById("forecast");
 
 // Get current weather data for a city
     //Display city name
