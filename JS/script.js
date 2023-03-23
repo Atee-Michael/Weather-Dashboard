acee8ff1f8b544ec3624425f093bedee4fedf317
unknown <ateemichael@yahoo.com>
2023-03-23 20:33:57 +0000

    retireve search history from local storage

--- JS/script.js
+++ JS/script.js
@@ -12,6 +12,16 @@ const searchHistory = document.getElementById("history");
 const todayWeather = document.getElementById("today");
 const forecastWeather = document.getElementById("forecast");
 
+// Create an empty array to store the search history
+let history = [];
+
+// Retrieve the search history from local storage
+if (localStorage.getItem("weatherHistory")) {
+  history = JSON.parse(localStorage.getItem("weatherHistory"));
+  showHistory(history);
+}
+
+
 // Get current weather data for a city
     //Display city name
     //Display Date
